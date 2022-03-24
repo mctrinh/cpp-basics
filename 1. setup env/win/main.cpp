17568cfffc1cedd2919328f1ef49c2362b6bfd8d
mctrinh <mctrinh@jbnu.ac.kr>
2022-03-24 09:28:04 +0900

    update tasks.json usage

--- 1. setup env/win/main.cpp
+++ 1. setup env/win/main.cpp
@@ -4,3 +4,6 @@ int main(){
     auto result = (10 <=> 20) > 0;
     std::cout << result << std::endl;
 }
+
+// tasks.json is only available when working on a workspace folder.
+//            is not available when editing single files.
