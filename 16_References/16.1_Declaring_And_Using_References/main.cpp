73f9b4a94fe4ea6b9b7d459c93650cbe76c3710d
mctrinh <mctrinh@jbnu.ac.kr>
2022-04-11 15:09:25 +0900

    add 16

---
+++ 16_References/16.1_Declaring_And_Using_References/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+int main(){
+
+    int int_data{33};
+    double double_data{55};
+
+    //References
+    int& ref_int_data{int_data};
+    double& ref_double_data{double_data};
+
+    //Print stuff out
+    std::cout << "int_data : " << int_data << std::endl;
+    std::cout << "&int_data : " << &int_data << std::endl;
+    std::cout << "double_data : " << double_data << std::endl;
+    std::cout << "&double_data : " << &double_data << std::endl;
+
+    std::cout << "=======================" << std::endl;
+
+    std::cout << "ref_int_data : " << ref_int_data << std::endl;
+    std::cout << "&ref_int_data : " << &ref_int_data << std::endl;
+    std::cout << "ref_double_data : " << ref_double_data << std::endl;
+    std::cout << "&ref_double_data : " << &ref_double_data << std::endl;
+
+    int_data = 111;         // new value on the same memory location
+    double_data = 67.2;     // new value the same memory location
+
+
+    //Print stuff out
+    std::cout << std::endl;
+    std::cout << "int_data : " << int_data << std::endl;
+    std::cout << "&int_data : " << &int_data << std::endl;
+    std::cout << "double_data : " << double_data << std::endl;
+    std::cout << "&double_data : " << &double_data << std::endl;
+
+    std::cout << "=======================" << std::endl;
+
+    std::cout << "ref_int_data : " << ref_int_data << std::endl;        // new value auto updated
+    std::cout << "&ref_int_data : " << &ref_int_data << std::endl;      // on the same memory location
+    std::cout << "ref_double_data : " << ref_double_data << std::endl;
+    std::cout << "&ref_double_data : " << &ref_double_data << std::endl;
+
+
+    ref_int_data = 1012;            // this auto update on original data
+    ref_double_data = 1000.45;      // this auto update on original data
+
+
+   //Print stuff out
+    std::cout << std::endl;
+    std::cout << "int_data : " << int_data << std::endl;
+    std::cout << "&int_data : " << &int_data << std::endl;
+    std::cout << "double_data : " << double_data << std::endl;
+    std::cout << "&double_data : " << &double_data << std::endl;
+
+    std::cout << "=======================" << std::endl;
+
+    std::cout << "ref_int_data : " << ref_int_data << std::endl;
+    std::cout << "&ref_int_data : " << &ref_int_data << std::endl;
+    std::cout << "ref_double_data : " << ref_double_data << std::endl;
+    std::cout << "&ref_double_data : " << &ref_double_data << std::endl;
+
+    return 0;
+}
