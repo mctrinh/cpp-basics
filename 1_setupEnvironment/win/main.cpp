b5e5bdbd621361dfd59f189554252dbbd2889c91
mctrinh <mctrinh@jbnu.ac.kr>
2022-08-09 23:01:51 +0900

    update

--- 1_setupEnvironment/win/main.cpp
+++ 1_setupEnvironment/win/main.cpp
@@ -8,3 +8,9 @@ int main(){
 
 // tasks.json is only available when working on a workspace folder.
 //            is not available when editing single files.
+// .vscode folder
+/*
+    task.json: build instructions
+    launch.json: debugger settings
+    c_cpp_properties.json: compiler path and IntelliSense settings
+*/
